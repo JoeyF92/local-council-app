73d3f9495aee573f1208fa0f6e86997b547deac7
JoeyF92 <jf-9294@protonmail.com>
2023-07-05 11:02:34 +0100

    updated get submission by status model

--- client/admin.js
+++ client/admin.js
@@ -1,16 +1,20 @@
+const { populate } = require("dotenv/lib/main");
+
 const loadProposals = async () => {
-  console.log("hi");
   const response = await fetch(
     "http://localhost:3000/submissions/status/pending"
   );
-  console.log("hi2");
-  const data = await response;
+  const data = await response.json();
 
   if (response.status == 200) {
-    alert("User created!");
+    populateTable(data);
   } else {
-    alert(data.error);
+    console.log(data.error);
   }
 };
+
+
+const populateTable(data){}
+
 
 loadProposals();
--- server/models/Submissions.js
+++ server/models/Submissions.js
@@ -1,56 +1,58 @@
-const db = require('../database/connect')
+const db = require("../database/connect");
 
-class Submissions{
+class Submissions {
+  static async getAllSubmissions() {
+    const query = "SELECT * FROM voting_submissions";
+    const { rows } = await db.query(query);
+    return rows;
+  }
 
-    static async getAllSubmissions(){
-        const query = 'SELECT * FROM voting_submissions'
-        const {rows} = await db.query(query)
-        return rows
-    }
+  static async getSubmissionById(id) {
+    const query = "SELECT * FROM voting_submissions WHERE submission_id = $1";
+    const { rows } = await db.query(query, [id]);
+    return rows[0];
+  }
 
-    static async getSubmissionById(id){
-        const query = 'SELECT * FROM voting_submissions WHERE submission_id = $1'
-        const {rows} = await db.query(query,[id])
-        return rows[0]
-    }
+  static async getSubmissionsByStatus(submission_status) {
+    const query =
+      "SELECT * FROM voting_submissions WHERE submission_status= $1";
+    const { rows } = await db.query(query, [submission_status]);
+    return rows;
+  }
 
-    static async getSubmissionsByStatus(submission_status){
-        const query = 'SELECT * FROM voting_submissions WHERE submission_status= $1'
-        const {rows} = await db.query(query,[submission_status])
-        return rows[0]
-    }
+  static async createSubmission(submission) {
+    const { title, category, proposal, photo } = submission;
+    const query =
+      "INSERT INTO voting_submissions (title, category, proposal, photo) VALUES ($1, $2, $3, $4) RETURNING *";
+    const values = [title, category, proposal, photo];
+    const { rows } = await db.query(query, values);
+    return rows[0];
+  }
 
-    static async createSubmission(submission){
-        const {title, category, proposal , photo} = submission
-        const query = 'INSERT INTO voting_submissions (title, category, proposal, photo) VALUES ($1, $2, $3, $4) RETURNING *'
-        const values = [title, category, proposal, photo]
-        const {rows} = await db.query(query, values)
-        return rows[0]
-    }
+  static async updateSubmission(id, submission) {
+    const { title, category, proposal, photo } = submission;
+    const query =
+      "UPDATE voting_submissions SET title = $1, category = $2, proposal = $3, photo = $4 WHERE submission_id = $5 RETURNING *";
+    const values = [title, category, proposal, photo, id];
+    const { rows } = await db.query(query, values);
+    return rows[0];
+  }
 
-    static async updateSubmission(id, submission){
-        const {title, category, proposal , photo} = submission
-        const query = 'UPDATE voting_submissions SET title = $1, category = $2, proposal = $3, photo = $4 WHERE submission_id = $5 RETURNING *'
-        const values = [title, category, proposal , photo, id]
-        const {rows} = await db.query(query, values)
-        return rows[0]
-    }
+  static async updateSubmissionStatus(id, submission) {
+    const { submission_status } = submission;
+    const query =
+      "UPDATE voting_submissions SET submission_status = $1 WHERE submission_id = $2 RETURNING *";
+    const values = [submission_status, id];
+    const { rows } = await db.query(query, values);
+    return rows[0];
+  }
 
-    static async updateSubmissionStatus(id, submission){
-        const {submission_status} = submission
-        const query = 'UPDATE voting_submissions SET submission_status = $1 WHERE submission_id = $2 RETURNING *'
-        const values = [submission_status, id]
-        const {rows} = await db.query(query, values)
-        return rows[0]
-    }
-
-    static async vote(count, id){
-        const query = `UPDATE voting_submissions SET votes = votes + $1 WHERE submission_id = $2`;
-        const values = [count, id];
-        const {rows} = await db.query(query, values)
-        return rows[0]
-    }
-
+  static async vote(count, id) {
+    const query = `UPDATE voting_submissions SET votes = votes + $1 WHERE submission_id = $2`;
+    const values = [count, id];
+    const { rows } = await db.query(query, values);
+    return rows[0];
+  }
 }
 
-module.exports = Submissions
+module.exports = Submissions;
